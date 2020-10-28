b897180c52c985e599f07a5b5c6ebb08a802dcee
Christian Compton <cbcompto@us.ibm.com>
2020-10-28 10:01:22 -0500

    chore: appscan annotations (#83)

--- v4/core/cp4d_authenticator.go
+++ v4/core/cp4d_authenticator.go
@@ -246,6 +246,7 @@ func (authenticator *CloudPakForDataAuthenticator) requestToken() (*cp4dTokenSer
 		// If the user told us to disable SSL verification, then do it now.
 		if authenticator.DisableSSLVerification {
 			transport := &http.Transport{
+				/* #nosec G402 */
 				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 			}
 			authenticator.Client.Transport = transport
